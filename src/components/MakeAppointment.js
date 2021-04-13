ea442bbffce61f2656936429f82bc1001edd9bf3
wing <wongwingchit@gmail.com>
2021-04-13 19:58:10 +0800

    successfully displayed occupied  appointment slot

--- src/components/MakeAppointment.js
+++ src/components/MakeAppointment.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import moment from 'moment';
-import ReactTimeslotCalendar from 'react-timeslot-calendar';
+import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 /**
 * @author
 * @function MakeAppointment
@@ -13,6 +13,7 @@ const MakeAppointment = (props) => {
   const[description, setDescription] = useState("");
   const[date, setDate] = useState(new Date());
   const[slot, setSlot] = useState("");
+  const[loadSlot, setLoadSlot] = useState({});
   
   const onChangePetName = (e) =>{
     setPetName(e.target.value);
@@ -30,9 +31,13 @@ const MakeAppointment = (props) => {
     setDate(e.target.value);
   }
   const onChangeSlot = (e) => {
+    console.log(e)
     setSlot(e.target.value);
   }
-
+  useEffect (() =>{
+    getSlots();
+    
+  },[])
   const submit = async () => {
     let id = localStorage.getItem("userId");
     const requestOptions = {
@@ -45,6 +50,20 @@ const MakeAppointment = (props) => {
     }
     const res = await fetch(`http://localhost:8080/api/${id}/appointment`, requestOptions);
     const res2 = handleResponse(res)
+  }
+  const getSlots = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json',
+        'Authorization': localStorage.getItem("user")
+      }
+    }
+    const res = await fetch(`http://localhost:8080/api/slots`, requestOptions);
+    const stuff = await res.json();
+
+    return setLoadSlot(stuff);
   }
 
   function handleResponse(response) {
@@ -69,9 +88,64 @@ const MakeAppointment = (props) => {
     });
   }
   
+  const handleScheduled = () => {
+    
+  };
+  
+  function timeSlotValidator(slotTime) {
+    const slotObj = loadSlot
+    let occupiedSlot_1 = new Date
+    let occupiedSlot_2 = new Date
+    let occupiedSlot_3 = new Date
+    for (let i=0; i<slotObj.length; i++){
+      let slotdayObj = new Date(loadSlot[i].day)
+      if(slotdayObj.getFullYear() == slotTime.getFullYear() && 
+         slotdayObj.getMonth() == slotTime.getMonth() &&
+         slotdayObj.getDate() == slotTime.getDate()){
+          console.log(loadSlot);
+          if(slotObj[i].slot_1){
+            occupiedSlot_1 = new Date(slotTime.getFullYear(), slotTime.getMonth(), slotTime.getDate(),
+              1,
+              0,
+              0
+            );
+            
+          }
+          
+          if(slotObj[i].slot_2){
+            occupiedSlot_2 = new Date(slotTime.getFullYear(), slotTime.getMonth(), slotTime.getDate(),
+              2,
+              0,
+              0
+            );
+            
+          }
+          if(slotObj[i].slot_3){
+            occupiedSlot_3 = new Date(slotTime.getFullYear(), slotTime.getMonth(), slotTime.getDate(),
+              3,
+              0,
+              0
+            );
+            
+         }
+      
+      }
+    }
+    
+  
+    const isValid = slotTime.getTime() != occupiedSlot_1.getTime() &&
+                    slotTime.getTime() != occupiedSlot_2.getTime() &&
+                    slotTime.getTime() != occupiedSlot_3.getTime()
+                    
+    
+    //  console.log(slotObj);
+    // console.log(slotTime.getDate());
+    return isValid;
+  }
 
   return(
-    <div className= "container justify-content-around" fluid={true}>
+    
+    <div className= "container justify-content-around" >
       <form onSubmit = {submit}>
         <h1 className="h3 mb-3 fw-normal">Make an Appointment</h1>
         
@@ -81,8 +155,11 @@ const MakeAppointment = (props) => {
         <input name="age" value = {age} onChange= {onChangeAge} type="text" className="form-control" placeholder="Age" required/>
         <input name="description" value = {description} onChange= {onChangeDescription} type="text" className="form-control" placeholder="Description" required/>
         <input name="date" value = {date} onChange= {onChangeDate} type="text" className="form-control" placeholder="Date" required/>
-        <ReactTimeslotCalendar
-          initialDate={moment().format()}
+        <DayTimePicker 
+          timeSlotSizeMinutes={60}
+          onConfirm={handleScheduled}
+          timeSlotValidator={timeSlotValidator}
+
         />
         <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
       </form>
