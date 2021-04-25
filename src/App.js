1449e6148ea1af41b91ae9c56ec104f9315e1d57
Hans Gehman <thehansgehman@gmail.com>
2021-04-24 23:46:55 -0500

    new geolocator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import './App.css';
 import Home from './components/Home';
@@ -17,9 +17,48 @@ import AllAppointments from './components/AllAppointments';
 
 
 
+
 function App() {
+
+  const [cities] = useState([
+    {
+        "location": "North American Office",
+        "climate": "land",
+        "species": "Arachnid, Cat, Dog, Ewok, Flerken, Houndeye, Old Man Yoda, Porg, Skunk, Tiger, Velociraptor, Vortigaunt, Warg",
+        "lat": "49",
+        "lng": "-120",
+        "address": "1507 Amerigo Vespucci Avenida"
+    },
+    {
+        "location": "Sahara Office",
+        "climate": "desert",
+        "species": "Bantha, Camel, Dewback, Jawa, Rancor",
+        "lat": "33",
+        "lng": "-3",
+        "address": "2 Sonson Tatoo Ine Road"
+    },
+    {
+        "location": "Indian Ocean Office",
+        "climate": "water",
+        "species": "Baby Snapping Turtle, Crawdad, Fish, Frog, Gooberfish, Grass Carp, Green Sunfish, Gungan, Ichthyosaur, Kaiju, Mink, Muskrat",
+        "lat": "-35",
+        "lng": "75",
+        "address": "501 Kamino Camino"
+    },
+    {
+        "location": "Antarctica Office",
+        "climate": "cold",
+        "species": "Penguin, Tauntaun, Wampa",
+        "lat": "-67",
+        "lng": "-75",
+        "address": "1986 Echo Boulevard"
+    }
+  ]
+  );
+
   return (
     <div className="App">
+      {/* <Geolocator mappedCities={cities}></Geolocator> */}
          <Router>
         <Nav/>
         
@@ -28,7 +67,8 @@ function App() {
           <Route path="/login" exact component={Login} />
           <Route path="/register" exact component={Register} />
           <Route path="/locations" exact component={Home} />
-          <Route path="/geolocator" component={Geolocator} />
+          {/* <Route path="/geolocator" component={Geolocator} /> */}
+          <Route exact path="/geolocator" render={(props) => <Geolocator {...props} mappedCities={cities} />} />
           <Route path="/about-us" component={AboutUs} />
           <Route path="/contact-us" component={ContactUs} />
           <Route path="/logout" component={Logout} />
